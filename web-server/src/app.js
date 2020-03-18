25b7107dfb6cb49f23fe3c5fd4c34fe0f07adcba
Fabio Pozzi <fabio@fabiopozzi.net>
2020-03-18 12:09:12 +0100

    Intro to express.js. Basic static pages setupped. First challenge solved.

---
+++ web-server/src/app.js
@@ -0,0 +1,23 @@
+const express = require('express')
+
+const app = express()
+
+app.get('', (req, res) => {
+    res.send('Hello express!')
+})
+
+app.get('/help', (req, res) => {
+    res.send('Help page.')
+})
+
+app.get('/about', (req, res) => {
+    res.send('This is the about page.')
+})
+
+app.get('/weather', (req, res) => {
+    res.send('Show weather page.')
+})
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000')
+})
