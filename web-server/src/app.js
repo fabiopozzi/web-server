366c72a44c685c23b3d7f0800487311e6deccccd
Fabio Pozzi <fabio@fabiopozzi.net>
2020-03-18 21:47:21 +0100

    install handlebars and hbs. Migrate HTML pages to hbs templates.

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -4,7 +4,29 @@ const express = require('express')
 const app = express()
 const publicDirPath = path.join(__dirname, '../public')
 
+app.set('view engine', 'hbs')
 app.use(express.static(publicDirPath))
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather',
+        name: 'Fabio Pozzi'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Me',
+        name: 'Fabio Pozzi'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help',
+        message: 'This page explains how to use the website'
+    })
+})
 
 app.get('/weather', (req, res) => {
     res.send({
