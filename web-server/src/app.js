752f167db8e8975310d222521966aed7c4287db6
Fabio Pozzi <fabio@fabiopozzi.net>
2020-03-19 08:30:41 +0100

    Configure handlebars to use partials. Add header and footer partials and refactor

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,15 +1,18 @@
 const path = require('path')
 const express = require('express')
+const hbs = require('hbs')
 
 const app = express()
 
 // Define path for Express configuration
 const publicDirPath = path.join(__dirname, '../public')
-const viewsPath = path.join(__dirname, '../templates')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
 
 // Setup handlebars engine and views location
 app.set('view engine', 'hbs')
 app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
 
 // setup static directory to use
 app.use(express.static(publicDirPath))
@@ -31,7 +34,8 @@ app.get('/about', (req, res) => {
 app.get('/help', (req, res) => {
     res.render('help', {
         title: 'Help',
-        message: 'This page explains how to use the website'
+        message: 'This page explains how to use the website',
+        name: 'Fabio Pozzi'
     })
 })
 
