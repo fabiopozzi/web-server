a7fe77826d894d4b5b5388323b1b4583b96a4a90
Fabio Pozzi <fabio@fabiopozzi.net>
2020-03-19 11:04:03 +0100

    Add custom 404 page. Handle some 404 cases.

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -46,6 +46,22 @@ app.get('/weather', (req, res) => {
     })
 })
 
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        title: '404 Error',
+        errorMessage: 'Help article not found',
+        name: 'Fabio Pozzi'
+    })
+})
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: '404 Error',
+        errorMessage: 'Page not found',
+        name: 'Fabio Pozzi'
+    })
+})
+
 app.listen(3000, () => {
     console.log('Server is up on port 3000')
 })
