432600f301993e888e77992ad965e693bc7cb483
Fabio Pozzi <fabio@fabiopozzi.net>
2020-03-20 18:56:00 +0100

    Add client-side js to fetch weather json from server and log to console.

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,11 @@
 console.log('Client side javascript file is loaded!')
+
+fetch('http://localhost:3000/weather?address=Novara').then((response) => {
+    response.json().then((data) => {
+        if (data.error) {
+            return console.log('Error: ' , data.error)
+        }
+        console.log('Location:', data.location)
+        console.log('Forecast:', data.forecast)
+    })
+})
