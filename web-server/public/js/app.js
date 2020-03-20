65b93384a940835cb28e7d92ec8502575ad25942
Fabio Pozzi <fabio@fabiopozzi.net>
2020-03-21 00:22:18 +0100

    Add form to send search term. Add clientJS code to fetch weather info. Add CSS styling for form and button.

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,11 +1,27 @@
-console.log('Client side javascript file is loaded!')
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
 
-fetch('http://localhost:3000/weather?address=Novara').then((response) => {
-    response.json().then((data) => {
-        if (data.error) {
-            return console.log('Error: ' , data.error)
-        }
-        console.log('Location:', data.location)
-        console.log('Forecast:', data.forecast)
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const location = search.value
+    const queryString = 'http://localhost:3000/weather?address=' + location
+
+    messageOne.textContent = 'loading, please wait...'
+    messageTwo.textContent = ''
+
+    fetch(queryString).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                messageOne.textContent = 'Error: '+ data.error
+            } else {
+                messageOne.textContent = 'Location: ' + data.location
+                messageTwo.textContent = 'Forecast: ' + data.forecast
+
+            }
+        })
     })
+
 })
