094d67fcb5a8b4dd39b1e7ab76745344f1bfa042
Fabio Pozzi <fabio@fabiopozzi.net>
2020-03-21 18:22:12 +0100

    Add some more weather infos. Translate to italian.

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,16 +1,18 @@
 const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {
-    const url = 'https://api.darksky.net/forecast/af375ff38c946c66aec4e47aa88fe561/' + latitude + ',' + longitude + '?units=si&lang=it'
+    const url = 'https://api.darksky.net/forecast/af375ff38c946c66aec4e47aa88fe561/' + latitude + ',' + longitude + '?units=ca&lang=it'
     request({ url, json: true }, (error, { body }) => {
         if (error) {
-            callback('unable to connect to weather service!', undefined)
+            callback('Impossibile collegarsi al servizio meteo', undefined)
         } else if (body.error) {
-            callback('unable to find location.', undefined)
+            callback('Impossibile trovare luogo', undefined)
         } else {
             const temp = body.currently.temperature
             const rain_chance = body.currently.precipProbability
-            callback(undefined, `Al momento ci sono ${temp}°C fuori. Le probabilità di pioggia sono ${rain_chance}%.`)
+            const hum = Number(body.currently.humidity) * 100
+            const wind = body.currently.windSpeed
+            callback(undefined, `Al momento ci sono ${temp}°C, umidità ${hum}%, vento ${wind} km/h. Le probabilità di pioggia sono ${rain_chance}%.`)
         }
     })
 }
